0514450c7eb67dc2d5c105b68cc9db6470a4773b
Jakub Otreba <jakub.otreba1@gmail.com>
2020-06-09 13:39:00 +0200

    Create Nav.js     update SearchBar.js

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import './Nav.sass';
+
+export default function Nav() {
+    return (
+        <div className="Nav">
+            <div className="Nav__logo">
+                <Link to='/' className = 'Nav__logo__link'>
+                    <i className='Nav__logo__link__icon fas fa-drumstick-bite'></i>
+                    <p className='Nav__logo__link__title'><span>R</span>ecipe<span>I</span>deas</p>
+                </Link>   
+            </div>
+            <div className="Nav__links">
+                <ul className="Nav__links__list">
+                    <Link to='/recipes' className="Nav__links__list__item">
+                        <li>Recipes</li>
+                    </Link>
+                    <Link to='/about' className="Nav__links__list__item">
+                        <li>About</li>
+                    </Link>
+                    <Link to='/blog' className="Nav__links__list__item">
+                        <li>Blog</li>
+                    </Link>
+                </ul>
+            </div>
+            
+        </div>
+    )
+}
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 
 import './SearchBar.sass';
 
@@ -7,30 +6,10 @@ export default function SearchBar({handleSubmit}) {
 
     return (
         <div className="SearchBar">
-            <div className="SearchBar__logo">
-                <Link to='/' className = 'SearchBar__logo__link'>
-                    <i className='SearchBar__logo__link__icon fas fa-drumstick-bite'></i>
-                    <p className='SearchBar__logo__link__title'><span>R</span>ecipe<span>I</span>deas</p>
-                </Link>   
-            </div>
             <form className='SearchBar__form' onSubmit={handleSubmit}>
                 <input type="text" name="query" className='SearchBar__form__input' />
-                <button type="submit" className='SearchBar__form__button'>Search</button>
+                <button type="submit" className='SearchBar__form__button'><i className="fas fa-search"></i></button>
             </form>
-            <div className="SearchBar__links">
-                <ul className="SearchBar__links__list">
-                    <Link to='/recipes' className="SearchBar__links__list__item">
-                        <li>Recipes</li>
-                    </Link>
-                    <Link to='/about' className="SearchBar__links__list__item">
-                        <li>About</li>
-                    </Link>
-                    <Link to='/blog' className="SearchBar__links__list__item">
-                        <li>Blog</li>
-                    </Link>
-                </ul>
-            </div>
-            
         </div>
         
     )

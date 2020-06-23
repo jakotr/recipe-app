611043ae21a04843b2735e249c202c57d8736abf
Jakub Otreba <jakub.otreba1@gmail.com>
2020-06-23 10:36:19 +0200

    Updates

---
+++ precache-manifest.c66214cbc8b9d1fe80e30d841c6e93eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb18ddff71dde08442524387a9d3b3a7",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "5600d5e1e300e7617248",
+    "url": "/recipe-app/static/css/main.d949b1f1.chunk.css"
+  },
+  {
+    "revision": "48d7802d7a3e8ed69234",
+    "url": "/recipe-app/static/js/2.ccf6eb97.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipe-app/static/js/2.ccf6eb97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5600d5e1e300e7617248",
+    "url": "/recipe-app/static/js/main.79c8416a.chunk.js"
+  },
+  {
+    "revision": "7ff5b27d3f9efd2f4352",
+    "url": "/recipe-app/static/js/runtime-main.96283c75.js"
+  },
+  {
+    "revision": "01f631bafec178e1d4957d15b84fef3f",
+    "url": "/recipe-app/static/media/intro_bg.01f631ba.png"
+  }
+]);
